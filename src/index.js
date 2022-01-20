bbd39e0b5b036d464a93d4f4d87d97f22c5a5bf6
Alex Gouvea Vasconcelos <alexgv@procempa.com.br>
2022-01-20 11:12:09 -0300

    starting (already useful)

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import wordsEn from "./dic-en.json";
+import wordsPt from "./dic-pt.json";
+
+const lettersIn = []; // letters to include with minimum quantity; ex: ["t1", "o2"]
+const lettersOut = "".split(""); // letters to exclude; ex: "bemf"
+const regex = /...../i; // put the letters you know the spot; ex: /...o./i
+
+function getWords(lang) {
+	let wordsOutput = [];
+	(lang === "pt" ? wordsPt : wordsEn)
+		.filter((word) => regex.test(word))
+		.filter((word) => {
+			return lettersIn.reduce((acc, ltr) => {
+				const [letter, min] = ltr.split("");
+				const re = new RegExp(letter, "g");
+				const qtd = (word.match(re) || []).length;
+				return acc && qtd >= min;
+			}, true);
+		})
+		.filter((word) =>
+			lettersOut.reduce(
+				(acc, letter) =>
+					acc && word.toLowerCase().indexOf(letter.toLowerCase()) === -1,
+				true
+			)
+		)
+		.forEach((word) => {
+			wordsOutput.push(word);
+		});
+	return wordsOutput;
+}
+
+console.log("palavras para https://term.ooo/: ", getWords("pt"));
+console.log(
+	"palavras para https://www.powerlanguage.co.uk/wordle/: ",
+	getWords("en")
+);
